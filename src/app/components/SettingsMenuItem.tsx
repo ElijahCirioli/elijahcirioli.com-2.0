5934de115651206e9469c7c689f175cb3455e56a
Elijah Cirioli <eli.cirioli@gmail.com>
2024-04-07 14:13:26 -0700

    slightly more progress on settings menu

---
+++ src/app/components/SettingsMenuItem.tsx
@@ -0,0 +1,34 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import styles from "./SettingsMenuItem.module.css";
+import { listClasses } from "../lib/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface SettingsMenuItemProps {
+	index: number;
+	text: string;
+	icon: IconProp;
+	visible: boolean;
+	updateSetting: () => void;
+}
+
+const SettingsMenuItem: React.FC<SettingsMenuItemProps> = ({
+	index,
+	text,
+	icon,
+	visible,
+	updateSetting,
+}: SettingsMenuItemProps) => {
+	return (
+		<div
+			className={visible ? styles.item : listClasses(styles.item, styles.hidden)}
+			style={{ top: `${visible ? 50 + 44 * index : 0}px` }}
+		>
+			<p>{text}</p>
+			<button onClick={updateSetting}>
+				<FontAwesomeIcon icon={icon} />
+			</button>
+		</div>
+	);
+};
+
+export default SettingsMenuItem;
