c3ce4ea41dffcaeaeef10e9b2feccaed625e4234
Elijah Cirioli <eli.cirioli@gmail.com>
2024-10-19 23:58:42 -0700

    very beginnings of mobile support

--- src/app/lib/UserSettings.ts
+++ src/app/lib/UserSettings.ts
@@ -9,7 +9,8 @@ export interface UserSettings {
 export function defaultUserSettings(): UserSettings {
 	return {
 		useDarkMode: false,
-		useReducedAnimations: window.matchMedia("(prefers-reduced-motion").matches,
+		useReducedAnimations: window.matchMedia("(prefers-reduced-motion: reduce), (max-width: 600px)")
+			.matches,
 	};
 }
 
